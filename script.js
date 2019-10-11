20a9066f7c67deac50722d4f91e56e74f73221e8
Krystal Farwig <youngshaowei@gmail.com>
2019-10-10 19:42:37 -0500

    finished the thing

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // TODO: add code here
+window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+      response.json().then(function(json) {
+      const container = document.getElementById('container')
+      for (i=0; i<json.length; i++) {
+      const containerInfo = 
+        `<div class="astronaut"> 
+            <div class="bio">
+                <h3>Astronaut ${json[i].firstName} ${json[i].lastName}</h3>
+                <ul>
+                <li>Hours in space: ${json[i].hoursInSpace}</li>
+                <li>Active: ${json[i].active}</li>
+                <li>Skills: ${json[i].skills}</li>
+                </ul>
+            </div>
+            <img class="avatar" src="${json[i].picture}">`;
+       container.innerHTML += containerInfo;     
+      }
+    })
+    });
+  })
